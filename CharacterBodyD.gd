dd149da642ac4d96953d05a9d9c61bf8c2faa274
Ole V. S. Mortensen <olevsmortensen@dbc5radio.dk>
2023-06-01 20:40:58 +0200

    Fix: Mouse input capture and camera node reference corrected

--- CharacterBodyD.gd
+++ CharacterBodyD.gd
@@ -1,42 +1,27 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var camera = $camera
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var gun = load("res://gun.tscn")
-@onready var camera =$camera
+var speed = 5
+var jump_speed = 5
+var mouse_sensitivity = 0.002
 
-func _unhandled_input(event: InputEvent):
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			rotate_y(-event.relative.x * .005)
-			camera.rotate_x(-event.relative.y * .005)
-			camera.rotation.x = clamp(camera.rotation.x, -PI/2, (PI/2))
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _physics_process(delta):
-	
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("Move_left", "Move_right ", "Move_forward", "Move_backward")
-	var direction = (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	velocity.y += -gravity * delta
+	var input = Input.get_vector("Move_left", "Move_right", "Move_forward", "Move_backward")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
 
 	move_and_slide()
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clampf(camera.rotation.x, -deg_to_rad(70), deg_to_rad(70))
